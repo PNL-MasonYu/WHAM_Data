a189ee873548e2f0710ff081ed1329bff4d3cf93
Cameron <6821396+CameronLP@users.noreply.github.com>
2023-08-10 14:56:56 -0500

    Update WhamRedPitaya
    Add connect, configure, arm, and read methods

--- WhamRedPitaya.py
+++ WhamRedPitaya.py
@@ -32,6 +32,9 @@ class WhamRedPitaya():
     STATUS_REG        = 0x000B_0008 # 2 bits : error_ACQ (STS =! 0x80) & data_tvalid_int ('1' when data transfer is done)
     START_ADDR_REG    = 0x000C_0000 # Min value is define by reserved memory in devicetree used to build Linux
 
+    dev = None
+    data_in = None
+
 
     def __init__(self, DEVICE_NODE="RP", IP="192.168.0.150", PORT=5000):
 
@@ -77,155 +80,165 @@ class WhamRedPitaya():
     def connect(self):
 
         print("Connecting to device at " + self.IP + " on port " + str(self.PORT))
-        dev = RP_PLL.RP_PLL_device(None)
-        dev.OpenTCPConnection(self.IP, self.PORT)
+        self.dev = RP_PLL.RP_PLL_device(None)
+        self.dev.OpenTCPConnection(self.IP, self.PORT)
 
+
+    def configure(self):
 
         # make sure n_pts is < MAXPOINTS
-        n_pts = min(self.MAXPOINTS,self.n_pts)
+        self.n_pts = min(self.MAXPOINTS,self.n_pts)
 
         # make sure downsample_value is between 1 and 2^32
-        downsample_value = min(2**32,downsample_value)
-        downsample_value = max(1,downsample_value)
+        self.downsample_value = min(2**32,self.downsample_value)
+        self.downsample_value = max(1,self.downsample_value)
 
-        dev.write_Zynq_AXI_register_uint32(self.DOWNSAMPLE_REG, downsample_value-1)
-        fs = fs/downsample_value
+        self.dev.write_Zynq_AXI_register_uint32(self.DOWNSAMPLE_REG, self.downsample_value-1)
+        self.fs = self.fs/self.downsample_value
+
+        #Reset DMA FSM (active low)
+        self.dev.write_Zynq_AXI_register_uint32(self.RESET_DMA_REG, 0)
+        self.dev.write_Zynq_AXI_register_uint32(self.RESET_DMA_REG, 1)
+
+        # set MUX
+        self.dev.write_Zynq_AXI_register_uint32(self.MUX_ADC_1_REG, self.ADC1_counter)
+        self.dev.write_Zynq_AXI_register_uint32(self.MUX_ADC_2_REG, self.ADC2_counter)
+
+        #set addr
+        self.dev.write_Zynq_AXI_register_uint32(self.START_ADDR_REG, self.data_addr)
+
+        # set n_bytes
+        n_bytes = int(2*self.n_pts)
+        n_bytes = max(n_bytes, 512) # at least 1 tx
+        self.dev.write_Zynq_AXI_register_uint32(self.N_BYTES_REG, n_bytes)
+
+        # set chan
+        self.dev.write_Zynq_AXI_register_uint32(self.CHANNEL_REG, self.channel)
+
+    def arm(self):
+
+        # set start
+        if self.bUseTrig == 0:
+            self.dev.write_Zynq_AXI_register_uint32(self.START_REG, 1)
+            self.dev.write_Zynq_AXI_register_uint32(self.START_REG, 0)
+        else:
+            self.dev.write_Zynq_AXI_register_uint32(self.TRIG_REG, 0) # 0 before to make sure we have a rising edge
+            self.dev.write_Zynq_AXI_register_uint32(self.TRIG_REG, 1) # Start with trig need to stay high to register external trig
+            self.dev.write_Zynq_AXI_register_uint32(self.TRIG_REG, 0)
+
+        time.sleep(self.n_pts/self.fs) 
 
         while self.bContinuous:
             
-            #Reset DMA FSM (active low)
-            dev.write_Zynq_AXI_register_uint32(self.RESET_DMA_REG, 0)
-            dev.write_Zynq_AXI_register_uint32(self.RESET_DMA_REG, 1)
-
-            # set MUX
-            dev.write_Zynq_AXI_register_uint32(self.MUX_ADC_1_REG, self.ADC1_counter)
-            dev.write_Zynq_AXI_register_uint32(self.MUX_ADC_2_REG, self.ADC2_counter)
-
-            #set addr
-            dev.write_Zynq_AXI_register_uint32(self.START_ADDR_REG, self.data_addr)
-
-            # set n_bytes
-            n_bytes = int(2*n_pts)
-            n_bytes = max(n_bytes, 512) # at least 1 tx
-            dev.write_Zynq_AXI_register_uint32(self.N_BYTES_REG, n_bytes)
-
-            # set chan
-            dev.write_Zynq_AXI_register_uint32(self.CHANNEL_REG, self.channel)
-
-            # set start
-            if self.bUseTrig == 0:
-                dev.write_Zynq_AXI_register_uint32(self.START_REG, 1)
-                dev.write_Zynq_AXI_register_uint32(self.START_REG, 0)
-            else:
-                dev.write_Zynq_AXI_register_uint32(self.TRIG_REG, 0) # 0 before to make sure we have a rising edge
-                dev.write_Zynq_AXI_register_uint32(self.TRIG_REG, 1) # Start with trig need to stay high to register external trig
-                dev.write_Zynq_AXI_register_uint32(self.TRIG_REG, 0)
-
-            time.sleep(n_pts/fs) 
-
             # read status (0b10 = error // 0b01 = data_valid // 0b00 = not_ready)
             status = 0
 
             while status == 0:
-                status = dev.read_Zynq_AXI_register_uint32(self.STATUS_REG)
+                status = self.dev.read_Zynq_AXI_register_uint32(self.STATUS_REG)
                 time.sleep(1e-1)
                 
-            print("Start receiving data")
-            timeStart = time.time()
-            data_in = dev.read_Zynq_ddr(self.data_addr-self.START_ADDR, int(2*n_pts))
-            print('Elapsed time for receiving= {}'.format(time.time() - timeStart))
+            self.__read()
 
             timeStart = time.time()
-            # data_in = np.fromstring(data_in, dtype=np.uint32) # Uncomment this line if you want to read data as 32 bits (useful if both ADCs are connected to counter)
-            data_in = np.fromstring(data_in, dtype=np.int16) # Uncomment this line if you want to read data as 16 bits
-            print('Elapsed time for conversion = {}'.format(time.time() - timeStart))
 
-            timeStart = time.time()
             if self.bSave == 1:
-                fNames = self.fileName.split(".")
-                fNames[0] += str(shot_num)
-                shot_num += 1
-                shot_name = fNames[0] + time.strftime("(%m-%d-%y;%H-%M-%S)") + "." + fNames[1]
-                hdf5_name = fNames[0] + ".hdf5"
-                print(shot_name)
-                if self.channel == 3 and not(self.ADC1_counter == 1 and self.ADC2_counter == 1):
-                    
-                    fileName_split = shot_name.rpartition('/')
-                    # write to HDF5
-                    """
-                    with h5py.File(hdf5_name, "w") as f:
-                        # create a group in the HDF5 file to store metadata
-                        metadata = f.create_group("metadata")
-                        
-                        # save the timestamp, acquisition frequency, and delay time as attributes of the metadata group
-                        metadata.attrs["timestamp"] = time.strftime("%m-%d-%y;%H-%M-%S")
-                        metadata.attrs["acq_freq"] = fs
-                        metadata.attrs["trig_delay"] = trig_delay
-                        
-                        # save the two arrays as datasets in the HDF5 file
-                        f.create_dataset("IN1", data=data_in[1::2].tobytes())
-                        f.create_dataset("IN2", data=data_in[::2].tobytes())
-                    """
-                    file_output = open(fileName_split[0] + fileName_split[1] +  'IN1_' + fileName_split[2], 'wb')
-                    file_output.write(data_in[1::2].tobytes())
-                    file_output.close()
-                    
-                    file_output = open(fileName_split[0] + fileName_split[1] +  'IN2_' + fileName_split[2], 'wb')
-                    file_output.write(data_in[::2].tobytes())
-                    file_output.close()
-                else:
-                    file_output = open(shot_name, 'wb')
-                    file_output.write(data_in.tobytes())
-                    file_output.close()
+                self.__write_hdf5()
 
             if self.bMDS == 1:
-                
-                conn = connection.Connection(self.mdsplus_server)      #Connect to Andrew
-                #tree_name = 230731000 + shot_num
-                
-                conn.openTree(self.mdsplus_tree, 0)
-
-                shot_num = conn.get('$shot') # Get current shot number using TDI expression
-                print("Writing data to shot number: " + shot_num)
-                
-                #result = conn.get("(RAW:ACQ196_370:CH_01)")
-                #print(result)
-
-                if self.channel == 3 and not(self.ADC1_counter == 1 and self.ADC2_counter == 1):
-                    
-                    #conn.put("RAW:RP_F0918A:CH_01", "$", np.int16(data_in[1::2]))
-                    #conn.put("RAW:RP_F0918A:CH_02", "$", np.int16(data_in[::2]))
-                    #conn.put("RAW:RP_F0918A:FREQ", "$", fs)
-
-                    conn.put(self.DEVICE_NODE+":CH_01", "$", np.int16(data_in[1::2]))
-                    conn.put(self.DEVICE_NODE+":CH_02", "$", np.int16(data_in[::2]))
-                    conn.put(self.DEVICE_NODE+":FREQ", "$", fs)
-                    conn.put(self.DEVICE_NODE+":NAME", "$", self.IP) # TODO: need to change this to IP
-
-
-                    #conn.put("ECH:ECH_RAW:RP_1:CH_01", "$", np.int16(data_in[1::2]))
-                    #conn.put("ECH:ECH_RAW:RP_1:CH_02", "$", np.int16(data_in[::2]))
-                    #conn.put("ECH:ECH_RAW:RP_1:FREQ", "$", fs)
-                    #conn.put("ECH:ECH_RAW:RP_1:NAME", "$", self.IP) # need to change this to IP
-
-
-                    conn.closeTree(self.mdsplus_tree, 0)
-                    #result = conn.get("(RAW:ACQ196_370:CH_01)")
-                    #print(result)
-                
+                self.__write_mdsplus()
 
             if self.bPlot == 1: 
-                if self.channel == 3:
-                    time_scale = np.linspace(0, 1/fs*len(data_in[::2]), len(data_in[::2]))
-                    plt.plot(time_scale, data_in[::2])
-                    plt.plot(time_scale, data_in[1::2])
-                    plt.show()
-                else:
-                    plt.plot(data_in)
-            #        plt.plot(np.diff(data_in))
-                    plt.show()
+                self.__write_plots()
 
-            
             print('Elapsed time for writing = {}'.format(time.time() - timeStart))
             print('Done')
 
+    def __read(self):
+        print("Start receiving data")
+        timeStart = time.time()
+        self.data_in = self.dev.read_Zynq_ddr(self.data_addr-self.START_ADDR, int(2*self.n_pts))
+        print('Elapsed time for receiving= {}'.format(time.time() - timeStart))
+
+        timeStart = time.time()
+        # self.data_in = np.fromstring(self.data_in, dtype=np.uint32) # Uncomment this line if you want to read data as 32 bits (useful if both ADCs are connected to counter)
+        self.data_in = np.fromstring(self.data_in, dtype=np.int16) # Uncomment this line if you want to read data as 16 bits
+        print('Elapsed time for conversion = {}'.format(time.time() - timeStart))
+        
+        
+
+
+    def __write_mdsplus(self):
+        conn = connection.Connection(self.mdsplus_server) # Connect to MDSplus server (andrew)
+        #tree_name = 230731000 + shot_num
+        
+        conn.openTree(self.mdsplus_tree, 0)
+
+        shot_num = conn.get('$shot') # Get current shot number using TDI expression
+        print("Writing data to shot number: " + shot_num) 
+        
+        if self.channel == 3 and not(self.ADC1_counter == 1 and self.ADC2_counter == 1):
+            conn.put(self.DEVICE_NODE+":CH_01", "$", np.int16(self.data_in[1::2]))
+            conn.put(self.DEVICE_NODE+":CH_02", "$", np.int16(self.data_in[::2]))
+            conn.put(self.DEVICE_NODE+":FREQ", "$", self.fs)
+            conn.put(self.DEVICE_NODE+":NAME", "$", self.IP) # TODO: need to change this to IP
+
+            #conn.put("RAW:RP_F0918A:CH_01", "$", np.int16(self.data_in[1::2]))
+            #conn.put("RAW:RP_F0918A:CH_02", "$", np.int16(self.data_in[::2]))
+            #conn.put("RAW:RP_F0918A:FREQ", "$", self.fs)
+
+            #conn.put("ECH:ECH_RAW:RP_1:CH_01", "$", np.int16(self.data_in[1::2]))
+            #conn.put("ECH:ECH_RAW:RP_1:CH_02", "$", np.int16(self.data_in[::2]))
+            #conn.put("ECH:ECH_RAW:RP_1:FREQ", "$", self.fs)
+            #conn.put("ECH:ECH_RAW:RP_1:NAME", "$", self.IP) # need to change this to IP
+
+            conn.closeTree(self.mdsplus_tree, 0)
+
+    
+    def __write_hdf5(self):
+        fNames = self.fileName.split(".")
+        fNames[0] += str(shot_num)
+        shot_num += 1
+        shot_name = fNames[0] + time.strftime("(%m-%d-%y;%H-%M-%S)") + "." + fNames[1]
+        hdf5_name = fNames[0] + ".hdf5"
+        print(shot_name)
+        if self.channel == 3 and not(self.ADC1_counter == 1 and self.ADC2_counter == 1):
+            
+            fileName_split = shot_name.rpartition('/')
+            # write to HDF5
+            """
+            with h5py.File(hdf5_name, "w") as f:
+                # create a group in the HDF5 file to store metadata
+                metadata = f.create_group("metadata")
+                
+                # save the timestamp, acquisition frequency, and delay time as attributes of the metadata group
+                metadata.attrs["timestamp"] = time.strftime("%m-%d-%y;%H-%M-%S")
+                metadata.attrs["acq_freq"] = fs
+                metadata.attrs["trig_delay"] = trig_delay
+                
+                # save the two arrays as datasets in the HDF5 file
+                f.create_dataset("IN1", data=self.data_in[1::2].tobytes())
+                f.create_dataset("IN2", data=self.data_in[::2].tobytes())
+            """
+            file_output = open(fileName_split[0] + fileName_split[1] +  'IN1_' + fileName_split[2], 'wb')
+            file_output.write(self.data_in[1::2].tobytes())
+            file_output.close()
+            
+            file_output = open(fileName_split[0] + fileName_split[1] +  'IN2_' + fileName_split[2], 'wb')
+            file_output.write(self.data_in[::2].tobytes())
+            file_output.close()
+        else:
+            file_output = open(shot_name, 'wb')
+            file_output.write(self.data_in.tobytes())
+            file_output.close()
+
+
+    def __write_plots(self):
+        if self.channel == 3:
+            time_scale = np.linspace(0, 1/self.fs*len(self.data_in[::2]), len(self.data_in[::2]))
+            plt.plot(time_scale, self.data_in[::2])
+            plt.plot(time_scale, self.data_in[1::2])
+            plt.show()
+        else:
+            plt.plot(self.data_in)
+    #        plt.plot(np.diff(self.data_in))
+            plt.show()
--- noGuiVersion2.py
+++ noGuiVersion2.py
@@ -3,11 +3,23 @@
 
 from WhamRedPitaya import WhamRedPitaya
 
-
+# MDSplus node to write data to
 RP1_NODE = "ECH:ECH_RAW:RP_1"
 
 rp = WhamRedPitaya(DEVICE_NODE=RP1_NODE, IP="192.168.0.150", PORT=5000)
 
+# Change any settings here
+rp.mdsplus_server = "andrew" # server running mdsplus
+rp.mdsplus_tree = "wham" # name of top mdsplus tree (should always be "wham")
+
+# Attempt to establish connection to the RP
 rp.connect()
+
+# Configure the RP device
+rp.configure()
+
+# Set the trigger on the RP and wait for data. 
+# Automatically reads and writes the data to MDSplus if it is recieved from the RP
+rp.arm()
 
 
