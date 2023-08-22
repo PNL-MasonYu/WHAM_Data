e4f5ce7b5b2fa8b6823e7e7b1efff6a46728c0b8
Cameron <6821396+CameronLP@users.noreply.github.com>
2023-08-22 10:07:26 -0500

    Update plotting script

--- plot.py
+++ plot.py
@@ -2,15 +2,11 @@
 from MDSmonkey import get_tree
 from matplotlib import pyplot as plt
 
+shot_num = 230818110
 
-#shot_num = 230818110
-shot_num = 0
+tree = get_tree(shot_num, "wham", "andrew.psl.wisc.edu")
 
-tree = get_tree(shot_num,"wham","andrew.psl.wisc.edu")
-
-#d = tree.ech.ech_raw.rp_01.ch_01.data
-#d = tree.raw.acq196_370.ch_01.data
-d = tree.raw.rp_f0918a.ch_01.data
+d = tree.ech.ech_raw.rp_01.ch_01.data
 
 d = d.to_numpy()
 plt.figure()
